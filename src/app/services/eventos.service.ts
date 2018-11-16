beb0101d84801b496a42e49c92dec91f5189cb9e
Graceje <greii_36@hotmail.com>
2018-11-15 20:08:23 -0700

    Update src/app/services/eventos.service.ts

--- src/app/services/eventos.service.ts
+++ src/app/services/eventos.service.ts
@@ -8,7 +8,11 @@ import { map, filter, switchMap } from 'rxjs/operators';
 export class EventosService {
 
   constructor(public _http:HttpClient) { }
+  getarea(){
+    return this._http.get('http://localhost/api-jardin/select.php');
+  }
   alta(eventos) {
     return this._http.post('http://localhost/api-jardin/insert_eventos.php', JSON.stringify(eventos));    
   }
+
 }
