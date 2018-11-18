02fc07937d6017faa001cdfd9bac292c435c94fc
Graceje <greii_36@hotmail.com>
2018-11-17 23:27:21 -0700

    jardin

--- src/app/correo/correo.component.ts
+++ src/app/correo/correo.component.ts
@@ -8,26 +8,71 @@ import {NotificacionesService} from '../services/notificaciones.service';
 })
 
 export class CorreoComponent implements OnInit {
-  
-  
+
+  notis:{
+    titulo:string,
+    institucion:string,
+    contacto:string,
+    adultos:string,
+    telefono:string,
+    correo:string,
+    fecha:string,
+    tiempo:string,
+    plantario: boolean,
+    mariposario:boolean,
+    Recinto_educativo:boolean,
+    Jardin_tematico:boolean
+ 
+  } 
 
   constructor(
      private router: Router,
     private route: ActivatedRoute,
-    private notificacionesservice: NotificacionesService ) { }
+    private notificacionesservice: NotificacionesService ) { 
+      this.notis = {
+        titulo:'',
+        institucion:'',
+        contacto:'',
+        adultos:'',
+        telefono:'',
+        correo:'',
+        fecha:'',
+        tiempo:'',
+        plantario: false,
+        mariposario: false,
+        Recinto_educativo: false,
+        Jardin_tematico:false
+      };
+    }
 
   ngOnInit() {
       this.getnotificacion();
+      
   }
-  notis;
+ 
   getnotificacion(){
     var id = this.route.snapshot.params['id'];
     this.notificacionesservice.notificacioness(id)
       .subscribe(notisk =>{
         console.log(notisk);
-        console.log("este")
           this.notis = notisk[0];
-          })
-  };
+          for (let i = 1; i < notisk.length+1; i++) {
+            const element = notisk[i];
+            if (element){
+              console.log(element.nombre)
+              if(element.nombre == 'Jardin_tematico'){
+                this.notis.Jardin_tematico=true;
+              } else if(element.nombre == 'plantario'){
+                this.notis.plantario=true;
+              }else if(element.nombre == 'mariposario'){
+                this.notis.mariposario=true;
+              }else if(element.nombre == 'recinto educativo'){
+                this.notis.Recinto_educativo=true;
+              }
+            }
+            
+          }
+  });
+  }
 
 }
